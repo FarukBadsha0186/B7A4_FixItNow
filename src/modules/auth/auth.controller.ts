990e53891c612ab91c2d014a6e284c6424f1f04b
FarukBadsha0186 <farukbadsha02@gmail.com>
2026-08-08 19:33:12 +0600

    Max age close

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -29,14 +29,14 @@ const loginUser = catchAsync(async (req: Request, res: Response) => {
         httpOnly: true,
         secure: false,
         sameSite: "none",
-        maxAge: 1000 * 60 * 60 * 24
+       // maxAge: 1000 * 60 * 60 * 24
     });
 
     res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
         secure: false,
         sameSite: "none",
-        maxAge: 1000 * 60 * 60 * 24 * 7
+      //  maxAge: 1000 * 60 * 60 * 24 * 7
     });
 
     sendResponse(res, {
